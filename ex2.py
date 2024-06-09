149e177439dec7741ddb6624f46c334b853c833a
Soh3ildaftari <soheilsoheili1113@gmail.com>
2024-06-09 19:56:14 +0330

    stars exercise done

---
+++ ex2.py
@@ -0,0 +1,4 @@
+#some tricky trick ;)
+stars='*********************************************************************************************'
+length=int(input('input:'))
+print('output',stars[0:length])
